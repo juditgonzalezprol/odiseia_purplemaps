3b77871f67747b2d2631a7f285b71230d15eb969
Judit <149954877+juditgonzalezprol@users.noreply.github.com>
2025-02-24 09:38:06 +0000

    V.2.4

--- pages/1_📍Mapa_Violeta.py
+++ pages/1_📍Mapa_Violeta.py
@@ -1,6 +1,7 @@
-import folium
-from streamlit_folium import st_folium
 import streamlit as st
+import folium
+from streamlit_folium import st_folium 
+
 
 st.set_page_config(page_title="Puntos Violeta", page_icon="🟣", layout="wide")
 
